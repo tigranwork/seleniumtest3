922e1754fee93aad80285b0b3f614271b2df7cb3
tigranwork <tigranwork@gmail.xom>
2020-07-05 22:08:18 +0400

    working_login

--- selenum lesson 1.py
+++ selenum lesson 1.py
@@ -1,12 +1,12 @@
 # Simple assignment
 from selenium.webdriver import Chrome
-driver = Chrome()
-with Chrome() as driver:
-    driver.get("http://the-internet.herokuapp.com/login")
-    username = driver.find_element_by_id('username')
-    username.send_keys('tomsmith')
-    password = driver.find_element_by_id('password')
-    password.send_keys('SuperSecretPassword!')
-    submit = driver.find_element_by_xpath('//*[@id="login"]/button')
+driver = Chrome('venv/Drivers/chromedriver.exe')
+with Chrome('venv/Drivers/chromedriver.exe') as driver:
+    driver.get("https://www.udemy.com/join/login-popup/?locale=en_US&response_type=html&next=https%3A%2F%2Fwww.udemy.com%2F")
+    username = driver.find_element_by_id('email--1')
+    username.send_keys('tigran.gagiyan@mail.ru')
+    password = driver.find_element_by_id('id_password')
+    password.send_keys('tigranarmen')
+    submit = driver.find_element_by_id('submit-id-submit')
     submit.click()
     driver.quit()
